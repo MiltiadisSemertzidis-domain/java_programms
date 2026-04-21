d92f7db8a76d4dd35bb5a5f416d908aa01aa842a
miltiadis semertzidis <miltous@miltos-debian.home>
2026-04-21 22:00:39 +0300

    start java journey

---
+++ java-basics/hello-world/Hello.java
@@ -0,0 +1,10 @@
+// This is the main class of the program
+public class Hello {
+
+    // The main method is the entry point of the program
+    public static void main(String[] args) {
+
+        // This line prints text to the console
+        System.out.println("Hello World!");
+    }
+}
